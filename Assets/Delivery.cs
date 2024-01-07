66c6fbee4d589ac078b603798b6d826eb1615a5c
mfhollif <mileshollifieldgfp@gmail.com>
2024-01-07 18:32:27 -0500

    Pushing project to GitHub

---
+++ Assets/Delivery.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    bool hasPackage;
+    [SerializeField] float destroyDelay = 0.5f;
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Watch where you're going!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Package" && !hasPackage) {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        } else if (other.tag == "Customer" && hasPackage) {
+            Debug.Log("Package delivered");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
